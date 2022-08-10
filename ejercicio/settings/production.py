841f449aae4993efe58c57f3c49d0adb19ed5a78
JustKike <jaircervantes11@gmail.com>
2022-08-09 19:26:30 -0700

    esto es una prueba de conexion herok2

--- ejercicio/settings/production.py
+++ ejercicio/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','ec2-3-225-110-188.compute-1.amazonaws.com']
 
